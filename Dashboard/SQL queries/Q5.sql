fdddb2c7d8653894f3c81bc8124bd77f285c41ed
Nitin Sharma <116897073+nitinsharma1706@users.noreply.github.com>
2023-07-24 18:08:39 +0530

    Update Q5.sql

--- Dashboard/SQL queries/Q5.sql
+++ Dashboard/SQL queries/Q5.sql
@@ -1,5 +1,7 @@
-"5.Count the number of jobs across different industry across different locations. For instance, how many Frontend Engineer jobs are there in Bangalore 
-as compared to Data Analytics jobs in Bangalore, or how many Data Analytics jobs are there in Bangalore as compared to number of Data Scientists job in Gurgaon - 
+"5.Count the number of jobs across different industry across different locations. For instance,
+  how many Frontend Engineer jobs are there in Bangalore 
+as compared to Data Analytics jobs in Bangalore, 
+  or how many Data Analytics jobs are there in Bangalore as compared to number of Data Scientists job in Gurgaon - 
 this needs to be done in SQL but presented in the above created Excel dashboard"
 
 select industry, location, designation, count(job_id) as cnt
