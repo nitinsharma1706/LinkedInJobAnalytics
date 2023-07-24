216f802e0b5253c4c6264f3d240f356e04ea9b57
Nitin Sharma <116897073+nitinsharma1706@users.noreply.github.com>
2023-07-24 18:01:48 +0530

    Update Q1.sql

--- Dashboard/SQL queries/Q1.sql
+++ Dashboard/SQL queries/Q1.sql
@@ -1,3 +1,30 @@
-" 1.Comparison of number of jobs across different cities for different level"
+How many job applicants have applied for each job posting, and
+  what are the required skills for the jobs that received the highest number of applicants?
+  
+  
+  "This question allows the business to identify which job postings are attracting the most applicants.
+  Knowing the popular job postings can help the HR department tailor their recruitment strategies
+  and allocate resources efficiently. Additionally, understanding the required skills 
+  for these popular jobs provides insights into the skillsets in high demand,
+which can guide the design of training programs or curricula to meet industry needs."
 
-select location, level, count(job_id) as cnt_job from jobs_table natural join detail_table group by location, level
+-- SQL Query
+  SELECT 
+    J."Job ID",
+    J."Job Title",
+    D."Required Skills",
+    J."Job Location",
+    D."Total Number of Job Applicants"
+FROM 
+    Jobs J
+JOIN Details D ON J."Details ID" = D."Details ID"
+WHERE 
+    D."Total Number of Job Applicants" = (
+        SELECT 
+            MAX("Total Number of Job Applicants")
+        FROM 
+            Details
+    );
+
+
+
