90b552f8abaf8c84be424832b4ab816c32ad0207
Nitin Sharma <116897073+nitinsharma1706@users.noreply.github.com>
2023-07-24 18:07:29 +0530

    Update Q2.sql

--- Dashboard/SQL queries/Q2.sql
+++ Dashboard/SQL queries/Q2.sql
@@ -1,8 +1,24 @@
-"2.Generate some insight with respect to number of jobs distribution across various industry. For instance, what is the total number of jobs in Software Industry as compared to Marketing"
+What are the top 5 job titles that have the highest number of job applicants, and which industries do these job titles belong to?
 
-select distinct(industry), count(job_id) as cnt_jobs from jobs_table natural join company_table group by industry
+  "This question helps the business identify the most sought-after job titles in the job market. For an ed-tech startup, 
+  this information can guide the development of relevant courses or programs to cater to popular job roles. College placement teams 
+  can leverage this data to focus on assisting students in securing internships or jobs in high-demand fields. Similarly, for HR departments,
+  understanding popular job titles in different industries aids in optimizing recruitment efforts to attract qualified candidates for specific roles."
 
-
+SELECT 
+    J."Job Title",
+    C."Industry",
+    SUM(D."Total Number of Job Applicants") AS "Total Applicants"
+FROM 
+    Jobs J
+JOIN Company C ON J."Company ID" = C."Company ID"
+JOIN Details D ON J."Details ID" = D."Details ID"
+GROUP BY 
+    J."Job Title",
+    C."Industry"
+ORDER BY 
+    SUM(D."Total Number of Job Applicants") DESC
+LIMIT 5;
 
 
 
